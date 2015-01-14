c596284181fda468d1d8e742f0995ff792e8d9ff
Skkeem <skkeem@gmail.com>
2015-01-14 15:35:10 +0900

    Broadcast Receiver, Intent service, Application added

--- src/com/example/makaotalk/Constants.java
+++ src/com/example/makaotalk/Constants.java
@@ -1,11 +1,12 @@
 package com.example.makaotalk;
 
 public class Constants {
-	public final static String TAG = "MAKAO";
+	public static final String TAG = "MAKAO";
 	public static final String[] CHAT_ROOMS = new String[] {};
+	public static final String SENDER_ID = "352935819908"; 
 	
 	public static class Extra {
-		public static final String FRAGMENT_INDEX = "com.teamd.proj1.FRAGMENT_INDEX";
+		public static final String FRAGMENT_INDEX = "com.example.makaotalk.FRAGMENT_INDEX";
 		public static final String CHAT_POSITION = "com.example.makaotalk.CHAT_POSITION";
 	}
 }
---
+++ src/com/example/makaotalk/MTApplication.java
@@ -0,0 +1,11 @@
+package com.example.makaotalk;
+
+import android.app.Application;
+
+public class MTApplication extends Application {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		
+	}
+}
